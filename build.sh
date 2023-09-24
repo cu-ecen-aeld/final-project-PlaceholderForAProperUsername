e9b86d4de18590f244cf334b6201c0c1eae850be
PlaceholderForAProperUsername <thorsten.hoffmeister@protonmail.com>
2023-09-24 14:12:19 +0200

    add meta-aeld_final_project

--- build.sh
+++ build.sh
@@ -13,12 +13,17 @@ source poky/oe-init-build-env
 add_configuration 'MACHINE = "raspberrypi4-64"'
 add_configuration 'LICENSE_FLAGS_ACCEPTED = "synaptics-killswitch"'
 add_configuration 'EXTRA_IMAGE_FEATURES += "debug-tweaks ssh-server-openssh"'
+add_configuration 'IMAGE_INSTALL:append = " libgpiod libgpiod-dev libgpiod-tools i2c-tools"'
+add_configuration 'ENABLE_I2C = "1"'
+add_configuration 'KERNEL_MODULE_AUTOLOAD:rpi += "i2c-dev i2c-bcm2708"'
+add_configuration 'MACHINE_EXTRA_RDEPENDS += "kernel-module-aeld_HD44780"'
 
 add_layer "meta-openembedded/meta-oe"
 add_layer "meta-openembedded/meta-python"
 add_layer "meta-openembedded/meta-networking"
 add_layer "meta-openembedded/meta-multimedia"
 add_layer "meta-raspberrypi"
+add_layer "meta-aeld_final_project"
 
 bitbake-layers show-layers
 
