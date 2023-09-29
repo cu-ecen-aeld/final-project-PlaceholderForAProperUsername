c234498ad13e1eb5685d03d286e5f515f6c0db93
PlaceholderForAProperUsername <thorsten.hoffmeister@protonmail.com>
2023-09-29 19:01:19 +0200

    update submodule

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ source poky/oe-init-build-env
 add_configuration 'MACHINE = "raspberrypi4-64"'
 add_configuration 'LICENSE_FLAGS_ACCEPTED = "synaptics-killswitch"'
 add_configuration 'EXTRA_IMAGE_FEATURES += "debug-tweaks ssh-server-openssh"'
-add_configuration 'IMAGE_INSTALL:append = " libgpiod libgpiod-dev libgpiod-tools i2c-tools kernel-module-aeldhd44780 aeldd aeldhd44780-start-stop kernel-module-aeldbme280 kernel-module-aeldbme280"'
+add_configuration 'IMAGE_INSTALL:append = " libgpiod libgpiod-dev libgpiod-tools i2c-tools kernel-module-aeldhd44780 aeldd aeldhd44780-start-stop kernel-module-aeldbme280"'
 add_configuration 'ENABLE_I2C = "1"'
 add_configuration 'KERNEL_MODULE_AUTOLOAD:rpi += "i2c-dev i2c-bcm2835"'
 
